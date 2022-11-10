fd526c00705f78674bd0bfad68671810e88a7f3f
SSteinmayer <samira.steinmayer@adesso.de>
2022-11-10 17:27:49 +0100

    added shared directory with divider component. Funcionality: Flexible Header

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from "../shared/shared.module";
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
@@ -11,7 +12,8 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
     ],
     imports: [
         CommonModule,
-        CollectionsRoutingModule
+        CollectionsRoutingModule,
+      SharedModule
     ]
 })
 export class CollectionsModule { }
