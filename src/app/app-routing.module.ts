15fa1cb70adfa31b865bee500d8ed04ac3837349
SSteinmayer <samira.steinmayer@adesso.de>
2022-11-10 15:01:22 +0100

    added lazy routing functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,19 @@ import {HomeComponent} from "./home/home.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 
 const routes: Routes = [
+  //lazy loading:
+  {
+    path: 'elements', loadChildren: () => import('./elements/elements.module')
+      .then(m => m.ElementsModule)
+  },
+  {
+    path: 'collections', loadChildren: () => import('./collections/collections.module')
+      .then(m => m.CollectionsModule)
+  },
+  {
+    path: 'mods', loadChildren: () => import('./mods/mods.module')
+      .then(m => m.ModsModule)
+  },
   { path: '', component: HomeComponent},
   { path: '**', component: NotFoundComponent}
 ];
